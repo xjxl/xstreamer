1573f31c2fd4e980a3d5270e2cbfa31ae3b11c53
xjl <lxj990@gmail.com>
2024-03-12 09:40:50 +0800

    LS

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char* argv[])
 	const char* localturnurl  = NULL;
 	const char* stunurl       = "stun.l.google.com:19302";
 	std::string localWebrtcUdpPortRange = "0:65535";
-	int logLevel              = rtc::LS_NONE;
+	int logLevel              = rtc::LS_ERROR;
 	const char* webroot       = "./www";
 	std::string sslCertificate;
 	webrtc::AudioDeviceModule::AudioLayer audioLayer = webrtc::AudioDeviceModule::kPlatformDefaultAudio;
@@ -151,7 +151,6 @@ int main(int argc, char* argv[])
 				}
 			break;			
 			case 'V':
-				std::cout << VERSION << std::endl;
 				exit(0);
 			break;
 			case 'h':
